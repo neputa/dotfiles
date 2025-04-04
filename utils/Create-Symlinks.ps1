2a530c090cec646db142533c0b9690c83aecb482
t.sasa <takuma.sasakubo@outlook.jp>
2025-04-05 05:17:30 +0900

    fix: fix symbolic link creation script :bug:

--- utils/Create-Symlinks.ps1
+++ utils/Create-Symlinks.ps1
@@ -8,13 +8,37 @@ function Create-Symlinks {
     Get-ChildItem -Directory $SourceFolder | ForEach-Object {
         $SourcePath = $_.FullName
         $TargetPath = Join-Path -Path $TargetFolder -ChildPath $_.Name
-        Create-Symlink -SourcePath $SourcePath -TargetPath $TargetPath
+
+        if (Test-Path $TargetPath) {
+            $TargetItem = Get-Item -Path $TargetPath -ErrorAction SilentlyContinue
+
+            # シンボリックリンクかどうかを確認
+            if ($TargetItem -and $TargetItem.Attributes -band [System.IO.FileAttributes]::ReparsePoint) {
+                Remove-Item -Path $TargetPath -Force
+            } else {
+                Rename-Item -Path $TargetPath -NewName "$($TargetPath).backup"
+            }
+        }
+
+        New-Item -ItemType SymbolicLink -Path $TargetPath -Target $SourcePath
     }
 
     # ファイルを対象にシンボリックリンクを作成
     Get-ChildItem -File -Path $SourceFolder -Filter ".*" | ForEach-Object {
         $SourcePath = $_.FullName
         $TargetPath = Join-Path -Path $HOME -ChildPath $_.Name
-        Create-Symlink -SourcePath $SourcePath -TargetPath $TargetPath
+
+        if (Test-Path $TargetPath) {
+            $TargetItem = Get-Item -Path $TargetPath -ErrorAction SilentlyContinue
+
+            # シンボリックリンクかどうかを確認
+            if ($TargetItem -and $TargetItem.Attributes -band [System.IO.FileAttributes]::ReparsePoint) {
+                Remove-Item -Path $TargetPath -Force
+            } else {
+                Rename-Item -Path $TargetPath -NewName "$($TargetPath).backup"
+            }
+        }
+
+        New-Item -ItemType SymbolicLink -Path $TargetPath -Target $SourcePath
     }
 }
