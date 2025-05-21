156675b75d2d73770a1f9e4d3ab2cb61cf87f94f
t.sasa-win-home <takuma.sasakubo@outlook.jp>
2025-05-21 16:29:41 +0900

    fix: :sparkles: update toggleterm plugins settings

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -78,6 +78,7 @@ return {
       persist_size = true,
       direction = "horizontal",
       -- direction = "vertical",
+      close_on_exit = true, -- close the terminal window when the process exits
       -- shell = vim.o.shell, -- change the default shell
       -- This field is only relevant if direction is set to 'float'
       -- float_opts = {
